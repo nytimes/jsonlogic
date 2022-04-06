671990a21f939d4c743256f3742fafc3c373cf60
Jeremiah Via <jeremiah.via@nytimes.com>
2022-04-06 14:20:13 -0700

    Version 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.nytimes/jsonlogic "0.1.2-SNAPSHOT"
+(defproject com.nytimes/jsonlogic "0.1.2"
   :description "JsonLogic for Clojure"
   :url "https://github.com/nytimes/jsonlogic"
   :license {:name "Apache License"
